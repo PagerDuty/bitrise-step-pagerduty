d2cd16e6ec7cdb257ebaf728150fee91cab1f6a1
Ryan Walberg <rwalberg@pagerduty.com>
2019-09-13 13:35:36 -0400

    add support for step outputs, fix defaults

--- step.sh
+++ step.sh
@@ -7,7 +7,7 @@ if [ $BITRISE_BUILD_STATUS == "0" ] && [ ${skip_if_build_is_passing} == "true" ]
 fi
 
 if [ ${api_version} == "v1" ]; then
-    curl -X POST https://events.pagerduty.com/generic/2010-04-15/create_event.json -d @- << EOF
+    curl -X POST https://events.pagerduty.com/generic/2010-04-15/create_event.json -o v1-output.json -d @- << EOF
     {
         "service_key": "${integration_key}",
         "event_type": "trigger",
@@ -22,13 +22,19 @@ if [ ${api_version} == "v1" ]; then
         }
     }
 EOF
+
+    PD_MESSAGE=$(cat v1-output.json | jq '.message')
+    PD_INCIDENT_KEY=$(cat v1-output.json | jq '.incident_key')
+    envman add --key PAGERDUTY_EVENT_MESSAGE --value "$PD_MESSAGE"
+    envman add --key PAGERDUTY_INCIDENT_KEY --value "$PD_INCIDENT_KEY"
+
 elif [ ${api_version} == "v2" ]; then
     if [ "" == "${severity}" ]; then
         echo "Severity is required for Events API v2."
         exit 1
     fi
 
-    curl -X POST https://events.pagerduty.com/v2/enqueue -d @- << EOF
+    curl -X POST https://events.pagerduty.com/v2/enqueue -o v2-output.json -d @- << EOF
     {
         "routing_key": "${integration_key}",
         "event_action": "trigger",
@@ -47,27 +53,13 @@ elif [ ${api_version} == "v2" ]; then
         }
     }
 EOF
+
+    PD_MESSAGE=$(cat v2-output.json | jq '.message')
+    PD_DEDUP_KEY=$(cat v2-output.json | jq '.dedup_key')
+    envman add --key PAGERDUTY_EVENT_MESSAGE --value "$PD_MESSAGE"
+    envman add --key PAGERDUTY_DEDUP_KEY --value "$PD_DEDUP_KEY"
+
 else
     echo "Unknown API version ${api_version}"
     exit 1
 fi
-
-set | grep BITRISE
-
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
-
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
